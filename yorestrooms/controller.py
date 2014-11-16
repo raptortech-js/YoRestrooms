3721e37791359070da03db7f6e321a5619fa4a06
Jackie Scholl <jackie.h.scholl@gmail.com>
2014-11-16 12:32:39 -0500

    Improve handling of base URLs
    Allow the refuge restrooms base url to be an environment variable and
    send links back based on current base URL

--- yorestrooms/controller.py
+++ yorestrooms/controller.py
@@ -1,5 +1,5 @@
 from yorestrooms import app
-from flask import request, render_template
+from flask import request, url_for, render_template
 #from API_KEY import api_token
 import os
 import requests
@@ -7,6 +7,12 @@ import requests
 api_token = os.environ['HEROKU_KEY']
 
 YO_API = "https://api.justyo.co/yo/"
+_refuge_restrooms_production = "http://refugerestrooms.org"
+
+if 'RR_SERVER' in os.environ:
+    REFUGE_RESTROOMS_BASE = os.environ[RR_SERVER]
+else:
+    REFUGE_RESTROOMS_BASE = _refuge_restrooms_production
 
 
 def send_yo(username, link):
@@ -14,6 +20,11 @@ def send_yo(username, link):
     requests.post(
         YO_API,
         data={'api_token': api_token, 'username': username, 'link': link})
+
+
+@app.context_processor
+def inject_refuge_restrooms_base():
+    return dict(rrbase = REFUGE_RESTROOMS_BASE)
 
 
 @app.route('/')
@@ -27,7 +38,7 @@ def handle_error(e):
     return render_template('404.html')
 
 
-@app.route('/noresult')
+@app.route('/nolocation')
 def noresult():
     return render_template('noresult.html')
 
@@ -37,14 +48,15 @@ def yo():
     """Handle callback request"""
     username = request.args.get('username')
     location = request.args.get('location')
+
+    URL_BASE = request.url_root[:-1] #trim trailing slash
     if location is None:
-        send_yo(username, 'http://yo-restrooms.herokuapp.com/noresult')
-        return 'No location'
-    
-    splitted = location.split(';')
-    latitude = splitted[0]
-    longitude = splitted[1]
-    link = ("http://www.refugerestrooms.org/restrooms?utf8=%E2%9C%93"
-            "&search=Current+Location&lat={0}&long={1}").format(latitude, longitude)
+        link = URL_BASE + url_for('noresult')
+    else:
+        splitted = location.split(';')
+        latitude = splitted[0]
+        longitude = splitted[1]
+        link = ("{0}/restrooms?utf8=%E2%9C%93&search=Current+Location&lat={1}"
+               "&long={2}").format(REFUGE_RESTROOMS_BASE, latitude, longitude)
     send_yo(username, link)
-    return 'OK'
+    return 'OK; sent link {0} to user {1}'.format(link, username)
